f239714c4e578f426ecc86ba3dc24449f04e78c0
Hayson <heyson@sina.cn>
2017-07-12 23:11:18 +0800

    finish 9-1

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,13 @@
 class Order < ApplicationRecord
+  before_create :generate_token
   belongs_to :user
-
+  has_many :product_lists
   validates :billing_name, :billing_address, :shipping_name, :shipping_address, presence: true
+
+
+    def generate_token
+      self.token = SecureRandom.uuid
+    end
+
+
 end
